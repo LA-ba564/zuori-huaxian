39ce05a57b9cc4aeee4d75845dad2e71c6ec111b
la-ba564 <la-ba564@users.noreply.github.com>
2026-08-06 15:44:32 +0800

    纸间: 修复 .face 不限高导致的长句溢出/无法滚动/纸张主题下方空白卡；缓存 v8

--- pwa-app/sw.js
+++ pwa-app/sw.js
@@ -1,5 +1,5 @@
 /* 纸间 · Service Worker：静态资源离线缓存 */
-var CACHE = "zuori-huaxian-v7";
+var CACHE = "zuori-huaxian-v8";
 var ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./data.js", "./icon.svg", "./manifest.webmanifest"];
 
 self.addEventListener("install", function (e) {
